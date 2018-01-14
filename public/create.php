efa69a96dd6661ca1d91b1ff1b9f254f25f9f481
Joao Campos <j.campos4g@gmail.com>
2018-01-14 15:15:15 -0500

    adds new users to test database

--- public/create.php
+++ public/create.php
@@ -1,4 +1,40 @@
-<?php include "templates/header.php"; ?>
+<?php include "templates/header.php";?>
+
+<?php
+if (isset($_POST['submit'])) {
+  require "../config.php";
+
+	try
+	{
+		$connection = new PDO($dsn, $username, $password, $options);
+
+
+    $new_user = array(
+    	"firstname" => $_POST['firstname'],
+    	"lastname"  => $_POST['lastname'],
+    	"email"     => $_POST['email'],
+    	"age"       => $_POST['age'],
+    	"location"  => $_POST['location']
+    );
+
+    $sql = sprintf(
+    		"INSERT INTO %s (%s) values (%s)",
+    		"users",
+    		implode(", ", array_keys($new_user)),
+    		":" . implode(", :", array_keys($new_user))
+    );
+
+    $statement = $connection->prepare($sql);
+    $statement->execute($new_user);
+
+	}
+
+	catch(PDOException $error)
+	{
+		echo $sql . "<br>" . $error->getMessage();
+	}
+}
+?>
 
 <form method="post">
 	<label for="firstname">First Name</label>
@@ -15,5 +51,7 @@
 </form>
 
 <a href="index.php">Back to home</a>
+
+
 
 <?php include "templates/footer.php"; ?>
